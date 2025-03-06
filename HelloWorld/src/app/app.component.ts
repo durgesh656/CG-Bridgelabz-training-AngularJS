56243812168fee670d9cb90c5579ff22ba50171b
durgesh656 <durgeshkushwaha656@gmail.com>
2025-03-06 21:10:07 +0530

    [Durgesh Kushwaha] Add: 'check username is valid or not if not then show error' added✨

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,OnChanges } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
@@ -13,7 +13,8 @@ export class AppComponent {
 
   imgUrl = "BL_logo_square_jpg.jpg";
   url = "https://www.bridgelabz.com";
-  username: String = '';
+  username: string = '';
+  nameError: string ='';
 
   ngOnInit():void{
     this.title = "Hello from BridgeLabz."
@@ -24,4 +25,15 @@ export class AppComponent {
     console.log("Save button is clicked!",$event);
     window.open(this.url, "_blank")    
   }
+    // Check input event to check the username at real time
+    onInput($event:any){
+      console.log("Change Event Occurred!",$event.data);
+      const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+      if(nameRegex.test(this.username)){
+        this.nameError ="";
+        return;
+      }
+      this.nameError = "Name is Incorrect!";
+      
+    }
 }
